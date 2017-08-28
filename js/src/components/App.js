01d771c3fc595c413918e35adffa50de8accdd26
Thomas Brüggemann <mail@thomasbrueggemann.com>
2017-08-28 22:39:26 +0200

    beginn with reminders notification

--- js/src/components/App.js
+++ js/src/components/App.js
@@ -2,21 +2,23 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Tabs from "./Tabs";
+import RemindersNotification from "./RemindersNotification";
 
 class App extends React.Component {
-    // RENDER
-    render() {
-        return (
-            <div className="window">
-                <Header history={this.props.history} />
-                <Tabs history={this.props.history} />
-                <div className="window-content">
-                    {this.props.children}
-                </div>
-                <Footer />
-            </div>
-        );
-    }
+	// RENDER
+	render() {
+		return (
+			<div className="window">
+				<RemindersNotification />
+				<Header history={this.props.history} />
+				<Tabs history={this.props.history} />
+				<div className="window-content">
+					{this.props.children}
+				</div>
+				<Footer />
+			</div>
+		);
+	}
 }
 
 export default App;
