a91134d4d672a67d04f17bb77b7ca4502e245d54
NickBusey <nickabusey@gmail.com>
2018-07-17 22:25:22 -0600

    Refreshing documents list when ordering changes

--- js/src/stores/DocumentsStore.js
+++ js/src/stores/DocumentsStore.js
@@ -35,7 +35,7 @@ class DocumentsStore {
     getDocumentsSuccess(result) {
 
 		// check if a reset is necessary
-		if(this.correspondent !== result.correspondent || this.tag !== result.tag) {
+		if(this.correspondent !== result.correspondent || this.tag !== result.tag || this.ordering !== result.ordering) {
 			this.resetDocuments();
 		}
 
@@ -44,6 +44,7 @@ class DocumentsStore {
         this.correspondent = result.correspondent;
         this.tag = result.tag;
 		this.isLoading = false;
+		this.ordering = result.ordering
 		this.next = result.data.next;
     }
 
