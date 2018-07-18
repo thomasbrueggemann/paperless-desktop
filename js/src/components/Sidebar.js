a9fbbe5ce02de6d159f3ee4e0325060fb24d3bf0
NickBusey <nickabusey@gmail.com>
2018-07-17 22:55:03 -0600

    eslint fix

--- js/src/components/Sidebar.js
+++ js/src/components/Sidebar.js
@@ -5,49 +5,49 @@ import SidebarCorrespondents from "./SidebarCorrespondents";
 import Select from "react-select";
 
 class Sidebar extends PaperlessComponent {
+	constructor(props) {
+		super(props);
+		this.state = {
+			selectValue: "-created"
+		};
+	}
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectValue: "-created"
-        };
-    }
+	updateValue(newValue) {
+		this.setState({
+			selectValue: newValue
+		});
+	}
 
-    updateValue (newValue) {
-        this.setState({
-            selectValue: newValue
-        });
-    }
+	onChange(state) {
+		this.setState(state);
+	}
 
-    onChange(state) {
-        this.setState(state);
-    }
-
-    // RENDER
-    render() {
-        var options = [{ value: "-created", label: "Document Date" }, { value: "name", label: "Alphabetical" }];
-        var clearable = false;
-        return (
-            <div className="pane pane-sm sidebar">
-                <Select
-                    id="order-select"
-                    ref={(ref) => { 
-                        this.select = ref;
-                    }}
-                    options={options}
-                    clearable={clearable}
-                    simpleValue
-                    name="selected-order"
-                    value={this.props.ordering}
-                    onChange={this.props.setOrdering}
-                />
-                <SidebarCorrespondents
-                    setCorrespondentFilter={this.props.setCorrespondentFilter}
-                />
-                <SidebarTags setTagFilter={this.props.setTagFilter} />
-            </div>
-        );
-    }
+	// RENDER
+	render() {
+		var options = [
+			{ value: "-created", label: "Document Date" },
+			{ value: "name", label: "Alphabetical" }
+		];
+		var clearable = false;
+		return (
+			<div className="pane pane-sm sidebar">
+				<Select
+					id="order-select"
+					ref={(ref) => {
+						this.select = ref;
+					}}
+					options={options}
+					clearable={clearable}
+					simpleValue
+					name="selected-order"
+					value={this.props.ordering}
+					onChange={this.props.setOrdering}
+				/>
+				<SidebarCorrespondents setCorrespondentFilter={this.props.setCorrespondentFilter} />
+				<SidebarTags setTagFilter={this.props.setTagFilter} />
+			</div>
+		);
+	}
 }
 
 export default Sidebar;
