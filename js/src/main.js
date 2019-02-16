6a71bb2eb40d32eb1898066e29a0cd255f7f6ea9
Thomas Brüggemann <mail@thomasbrueggemann.com>
2019-02-16 12:26:25 +0100

    reset branch

--- js/src/main.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Router, Route, hashHistory } from "react-router";
-import ReactDOM from "react-dom";
-import routes from "./routes";
-
-ReactDOM.render(
-    <Router history={hashHistory}>{routes}</Router>,
-    document.getElementById("app")
-);
